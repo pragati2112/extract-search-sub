e4109bbcb9e403a0bd7f3180e3373524a24b5afe
pragati2112 <pandeypragati2112@gmail.com>
2022-09-08 11:44:53 +0530

    make video_id the partition key

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import datetime
 import shutil
 from pathlib import Path
-from fastapi import File, UploadFile, APIRouter, BackgroundTasks, Body
+from fastapi import File, UploadFile, APIRouter, BackgroundTasks, Body, jsonable_encoder
 import math
+import os
 from extraction import extract_subtitles
 from read_srt import read_srt_file, search_subtitle
 from worker import celery_app
-
 router = APIRouter()
 
 
@@ -14,23 +14,34 @@ def upload_video(file_path, filename, bucket_name):
     pass
 
 
+def generate_desired_directories():
+    desired_directory_path = 'uploads' + os.sep + 'srts'
+    if not os.path.exists(desired_directory_path):
+        os.makedirs(desired_directory_path)
+
+
 @router.post("/upload/video", response_description="Upload video")
-async def media(background_task: BackgroundTasks, file: UploadFile = File(...)):
-    extension = file.split('.')[-1]
+async def media(background_task: BackgroundTasks, video_file: UploadFile = File(...)):
+    extension = video_file.filename.split('.')[-1]
     new_file_name = str(math.floor(datetime.datetime.now().timestamp()))
     filename = new_file_name + '.' + extension
 
+    generate_desired_directories()
+
     destination = Path('uploads/' + filename)
     with destination.open("wb") as buffer:
-        shutil.copyfileobj(file, buffer)
+        shutil.copyfileobj(video_file.file, buffer)
 
     file_path = 'uploads/' + filename
 
     # send task to celery to upload video OR can use background tasks of fast api--
+
     # task_name = 'upload_video'
     # celery_app.send_task(task_name, args=[file_path, upload_bucket])
-
     background_task.add_task(upload_video(file_path, filename, bucket_name=''))
+
+    # close the file
+    video_file.file.close()
 
     # use cc-extractor--  to extract subtitles
     out_path = 'uploads/srts/' + new_file_name + '.srt'
@@ -44,4 +55,5 @@ async def media(background_task: BackgroundTasks, file: UploadFile = File(...)):
 
 @router.post("/search/subtitles", response_description="Search for subtitles")
 async def search(data: Body(...)):
+    data = jsonable_encoder(data)
     return search_subtitle(data['subtitle'], data['video_id'])
--- read_srt.py
+++ read_srt.py
@@ -1,10 +1,7 @@
 import pysrt
-import boto3
 from boto3.dynamodb.conditions import Key
-
-db = boto3.resource("dynamodb", endpoint_url="http://localhost:8000",
-                    region_name="dummy", aws_access_key_id="dummy", aws_secret_access_key="dummy")
-table = db.Table('Videos')
+from db_connection import create_videos_table
+videos_table = create_videos_table()
 
 
 def read_srt_file(file='uploads/srts/out.srt', video_id='123456789'):
@@ -13,19 +10,19 @@ def read_srt_file(file='uploads/srts/out.srt', video_id='123456789'):
         text = sub.text.strip('\n').strip('\t')
         new_text = text.replace('\n', "").replace('\t', "").lstrip()
         data = {'subtitles': new_text, 'videoId': video_id, 'start': str(sub.start), 'end': str(sub.end)}
-        table.put_item(Item=data)
+        videos_table.put_item(Item=data)
 
-    print('successfully insert')
+    print('successfully insert!')
     return True
 
 
 def search_subtitle(subtitle: str = '', video_id: str = '123456789'):
-    # response = table.scan(TableName='Videos')
+    # response = videos_table.scan(TableName='Videos')
     # items = response['Items']
     # for item in items:
     #     print(item)
 
-    resp = table.get_item(Key={'subtitles': subtitle, 'videoId': video_id})
+    resp = videos_table.get_item(Key={'subtitles': subtitle, 'videoId': video_id})
     if 'Item' in resp:
         print(resp['Item'])
         return resp['Item']
@@ -35,4 +32,4 @@ def search_subtitle(subtitle: str = '', video_id: str = '123456789'):
 
 
 # read_srt_file()
-search_subtitle("I'D CALL THAT")
+# search_subtitle("I'D CALL THAT")
